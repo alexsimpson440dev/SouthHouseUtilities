6a5a564978f3384d41aa9208043ba288f22878ab
Alex <Hello-Git@gmail.com>
2016-12-06 14:13:05 -0600

    Added UtilityDetailFragment.java and an xml file for that class

---
+++ app/src/main/java/com/example/me5013zu/southhouseutilities/UtilityDetailFragment.java
@@ -0,0 +1,11 @@
+package com.example.me5013zu.southhouseutilities;
+
+import android.app.Fragment;
+
+/**
+ * Created by me5013zu on 12/6/16.
+ */
+
+public class UtilityDetailFragment extends Fragment {
+
+}
